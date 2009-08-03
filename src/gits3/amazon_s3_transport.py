211f441c02c576b2bd5e6b344d5325761c083feb
Abdelhalim Ragab <abdelhalim@gmail.com>
2009-08-03 11:18:09 -0700

    Add copyright note

--- src/gits3/amazon_s3_transport.py
+++ src/gits3/amazon_s3_transport.py
@@ -1,5 +1,22 @@
+# Copyright (C) 2009 Abdelhalim Ragab <abdelhalim@gmail.com>
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; version 3
+# of the License or (at your option) any later version of 
+# the License.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
 '''
-Created on Jul 31, 2009
 
 @author: abdelhalim
 '''
